8a502a30f8c41facb4f09e0294cbbfae270871ec
Ketevan Karchava <ketevan0915@gmail.com>
2024-11-29 19:41:05 -0700

    Continued Practice about nested for loop

--- src/practice/Practice1.java
+++ src/practice/Practice1.java
@@ -4,6 +4,10 @@ public class Practice1 {
 
     public static void main(String[] args) {
 
+        String s1 = "wooden spoon";
+        String s2 = s1.substring(s1.indexOf("s"));
+        System.out.println(s2);
+
 
     }
 
